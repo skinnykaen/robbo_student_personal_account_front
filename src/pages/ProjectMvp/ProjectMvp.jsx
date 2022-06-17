f1bd5b6b0afa359c93b711ec3e816482034d265e
Evgeniy Sushchevich <rupychman@mail.ru>
2022-06-17 13:55:40 +0300

    page with link to mvp

--- src/pages/ProjectMvp/ProjectMvp.jsx
+++ src/pages/ProjectMvp/ProjectMvp.jsx
@@ -2,10 +2,19 @@ import React, { useState, useEffect } from "react";
 
 
 import { PageLayout } from "@/layouts";
+import { Card, MainContainer, WelcomeText, HardcoreLink } from "./components";
+import SideBar from "@/components/SideBar";
 
 export default () => {
     return (
         <PageLayout>
+            <Card>
+                <SideBar />
+                <MainContainer>
+                    <WelcomeText>Cсылка на Mvp</WelcomeText>
+                    <HardcoreLink href="http://0.0.0.0:8601/">Ссылка</HardcoreLink>
+                </MainContainer>
+            </Card>
         </PageLayout>
     )
 }

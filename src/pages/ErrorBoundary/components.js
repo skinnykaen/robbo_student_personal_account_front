21f933766c44c3374304914daf31a30ded382f15
arsenicxz <57327489+arsenicxz@users.noreply.github.com>
2022-07-15 17:40:10 +0300

    feat: add error boundary component

---
+++ src/pages/ErrorBoundary/components.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+export const Title = styled.p`
+    display: flex;
+    font-size: 3rem;
+    font-weight: bold;
+    margin-bottom: 1rem;
+`
+
+export const Code = styled.h1`
+    display: flex;
+    font-size: 5rem;
+    font-weight: bold;
+    margin-bottom: 1rem;
+`

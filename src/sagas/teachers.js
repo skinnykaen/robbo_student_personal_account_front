2d3a23e47f168902556023badc7d6c12ee2efe54
Evgeniy Sushchevich <rupychman@mail.ru>
2022-07-21 13:27:38 +0300

    feat: teachers

--- src/sagas/teachers.js
+++ src/sagas/teachers.js
@@ -1,4 +1,4 @@
-import { call, put, takeLatest } from 'redux-saga/effects'
+import { call, takeLatest } from 'redux-saga/effects'
 
 import { teachersAPI } from '@/api'
 import { getTeachers } from '@/actions/teachers'

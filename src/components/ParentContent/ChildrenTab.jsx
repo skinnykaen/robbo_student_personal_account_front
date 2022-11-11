31cb63dc7d593ff3bae93df0a0ca085b45f43ef8
Evgeniy Sushchevich <rupychman@mail.ru>
2022-11-11 20:05:46 +0300

    feat: refactor parent content

--- src/components/ParentContent/ChildrenTab.jsx
+++ src/components/ParentContent/ChildrenTab.jsx
@@ -1,11 +1,15 @@
-import React, { useState } from 'react'
-import { Space, Button, Modal, List } from 'antd'
+import React, { useEffect, useState } from 'react'
+import { Space, Button, Modal, List, Input, Card } from 'antd'
 import { LoadingOutlined } from '@ant-design/icons'
-import { gql, useQuery } from "@apollo/client"
+import { gql, useQuery, useLazyQuery, useApolloClient } from "@apollo/client"
+
+import { race } from 'redux-saga/effects'
 
 import ListItem from '@/components/ListItem'
 import AddChildren from '@/components/AddChildren'
 import { useActions } from '@/helpers'
+
+const { Search } = Input
 
 const GET_STUDENT_BY_PARENT_ID = gql`
 query GetStudentsByParentId($parentId: String!){
@@ -17,28 +21,71 @@ query GetStudentsByParentId($parentId: String!){
             middlename
         }
     }
-}
-`
+}`
+
+const SEARCH_STUDENTS_BY_EMAIL = gql`
+query SearchStudentsByEmail($email: String!) {
+    SearchStudentsByEmail(email: $email) {
+        userHttp{
+            id
+            email
+            lastname
+            firstname
+            middlename
+        }
+    }
+}`
+
 const ChildrenTab = ({ clientId }) => {
     const token = localStorage.getItem('token')
+    const client = useApolloClient()
     const {
         deleteChildRequest,
     } = useActions()
     const [openAddChildren, setOpenAddChildren] = useState(false)
     const [openSearchSection, setOpenSearchSection] = useState(false)
+    const [searchStudents, setSearchResult] = useState([])
+    const [students, setStudents] = useState([])
 
-    const { loading, error, data } = useQuery(GET_STUDENT_BY_PARENT_ID, {
+
+    const getStudentsResult = useQuery(GET_STUDENT_BY_PARENT_ID, {
         variables: { parentId: clientId },
+        notifyOnNetworkStatusChange: true,
     })
 
+    // const [searchStudents, searchRequest] = useLazyQuery(SEARCH_STUDENTS_BY_EMAIL, {
+    //     variables: { email },
+    //     notifyOnNetworkStatusChange: true,
+    // })
+
+    const GetStudentsByParentId = async clientId => {
+        const result = await client.query({
+            query: GET_STUDENT_BY_PARENT_ID,
+            variables: { parentId: clientId },
+        })
+        setStudents(result.data.GetStudentsByParentId)
+        console.log(result)
+    }
+
+    const SearchStudents = async value => {
+        const result = await client.query({
+            query: SEARCH_STUDENTS_BY_EMAIL,
+            variables: { email: value },
+        })
+        console.log("search")
+        console.log(result)
+        setSearchResult(result.data.SearchStudentsByEmail)
+    }
+
     return (
-        <React.Fragment>
+        // <React.Fragment>
+        <Space direction='vertical' style={{ margin: '0.5rem' }}>
             {
-                loading
+                getStudentsResult.loading
                     ? <LoadingOutlined />
                     : <List
                         bordered
-                        dataSource={data.GetStudentsByParentId}
+                        dataSource={getStudentsResult.data.GetStudentsByParentId}
                         renderItem={({ userHttp }, index) => (
                             <ListItem
                                 itemIndex={index}
@@ -51,8 +98,9 @@ const ChildrenTab = ({ clientId }) => {
                     />
             }
 
-            <Space>
-                <Button type='primary'>Добавить</Button>
+
+            <React.Fragment>
+                <Button type='primary' onClick={() => setOpenSearchSection(!openSearchSection)}>Добавить</Button>
                 <Button type='primary' onClick={setOpenAddChildren}>Создать </Button>
                 <Modal
                     title='Заполните данные ученика'
@@ -63,8 +111,29 @@ const ChildrenTab = ({ clientId }) => {
                 >
                     <AddChildren parentId={clientId} />
                 </Modal>
-            </Space>
-        </React.Fragment>
+            </React.Fragment>
+
+            {
+                openSearchSection &&
+                <React.Fragment>
+                    <Search placeholder='Введите Email' onSearch={SearchStudents}
+enterButton />
+                    < List
+                        bordered
+                        dataSource={searchStudents}
+                        renderItem={({ userHttp }, index) => (
+                            <ListItem
+                                itemIndex={index}
+                                key={index}
+                                render={() => { }}
+                                label={`${userHttp.lastname} ${userHttp.firstname} ${userHttp.middlename}`}
+                            // handleDelete={childIndex => deleteChildRequest(token, userHttp.id, childIndex)}
+                            />
+                        )}
+                    />
+                </React.Fragment>
+            }
+        </Space>
     )
 }
 
--- src/components/ParentContent/ParentContent.jsx
+++ src/components/ParentContent/ParentContent.jsx
@@ -82,80 +82,8 @@ export default ({ clientId }) => {
                             },
                         ]}
                     />
-
-                    {/* <DragResize
-                        open={openAddChildren} setOpen={setOpenAddChildren}
-                        width='35%' height='60%'
-                        content={() => (
-                            <AddChildren parentId={clientId} />
-                        )}
-                    />
-                    <Flex width='100%' justify='flex-end'>
-                        <Button
-                            content='Добавить ребенка из уже существующих'
-                            background='darkgreen'
-                            padding='0.5rem'
-                            width='40%'
-                            margin='0 1rem 0 0'
-                            handleSubmit={() => setOpenSearchSection(!openSearchSection)}
-                        />
-                        <Button
-                            content='Создать ребенка'
-                            background='darkgreen'
-                            padding='0.5rem'
-                            width='20%'
-                            handleSubmit={() => setOpenAddChildren(true)}
-                        />
-                    </Flex>
-                    {openSearchSection &&
-                        <Flex
-                            width='100%' margin='1rem 0 0 0'
-                            direction='column'
-                        >
-                            <SearchInput
-                                searchHandle={input => { searchStudent(token, input) }}
-                                placeholder='Введите Email'
-                            />
-                            <Flex direction='column'>
-                                {
-                                    searchResult ? (
-                                        searchResult?.map(({ userHttp }, index) => {
-                                            return (
-                                                <ListItem
-                                                    itemIndex={index}
-                                                    label={`${userHttp.lastname} ${userHttp.firstname} ${userHttp.middlename}`}
-                                                    key={index}
-                                                    render={() => { }}
-                                                    handleClick={() => createRelation(token, client.userHttp.id, userHttp.id)}
-                                                    handleDelete={false}
-                                                />
-                                            )
-                                        })
-                                    ) : "Ничего не найдено"}
-                            </Flex>
-                        </Flex>
-                    } */}
                 </Flex>
             </Flex>
-            {/* <SubTitle>Дети</SubTitle>
-            <ListChildrens>
-                {
-                    childrenLoading ? <Loader />
-                        : (
-                            children?.map(({ userHttp }, index) => {
-                                return (
-                                    <ListItem
-                                        itemIndex={index}
-                                        label={`${userHttp.lastname} ${userHttp.firstname} ${userHttp.middlename}`}
-                                        key={index}
-                                        render={() => { }}
-                                        handleDelete={childIndex => deleteChildRequest(token, userHttp.id, childIndex)}
-                                    />
-                                )
-                            })
-                        )
-                }
-            </ListChildrens> */}
         </Flex >
     )
 } 
